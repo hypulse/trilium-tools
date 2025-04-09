6e849b480619a845d637e7c5cdebcca0e434c106
Seungjae <sjinsilval28@gmail.com>
2025-04-09 21:54:02 +0900

    first commit

---
+++ newAtomNote.js
@@ -0,0 +1,18 @@
+api.addButtonToToolbar({
+  title: "New Atom Note",
+  icon: "atom",
+  shortcut: "alt+n",
+  action: async () => {
+    const newAtomNoteId =
+      await api.runAsyncOnBackendWithManualTransactionHandling(async () => {
+        const atomRootNote = await api.getNoteWithLabel("AtomNotesRoot");
+        const today = new Date().toLocaleString();
+
+        const { note } = await api.createNote(atomRootNote.noteId, today, "");
+        note.setLabel("iconClass", "bx bx-atom", false);
+
+        return note.noteId;
+      });
+    await api.activateNewNote(newAtomNoteId);
+  },
+});
